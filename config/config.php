6a309b27735d019da0d6193eb01b5beed4f6f09a
GitHub Action <action@github.com>
2023-06-08 05:42:21 +0000

    Pass param to Differ

--- config/config.php
+++ config/config.php
@@ -3,12 +3,15 @@
 declare(strict_types=1);
 
 use SebastianBergmann\Diff\Differ;
+use SebastianBergmann\Diff\Output\UnifiedDiffOutputBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\PackageBuilder\Console\Formatter\ColorConsoleDiffFormatter;
 
 use Symplify\PackageBuilder\Console\Output\ConsoleDiffer;
 use Symplify\PackageBuilder\Diff\Output\CompleteUnifiedDiffOutputBuilderFactory;
 use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -22,6 +25,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ConsoleDiffer::class);
     $services->set(CompleteUnifiedDiffOutputBuilderFactory::class);
 
-    $services->set(Differ::class);
+    // @comment-by-Leo Replaced this line with the code below
+    // $services->set(Differ::class);
+    $services->set(UnifiedDiffOutputBuilder::class)
+        ->factory([service(CompleteUnifiedDiffOutputBuilderFactory::class), 'create']);
+    $services->set(Differ::class)
+        ->arg('$outputBuilder', service(UnifiedDiffOutputBuilder::class));
+
     $services->set(PrivatesAccessor::class);
 };
